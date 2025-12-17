c3f57663f738440e08845e0882cc11429131c44b
Maksym Diachok <maksdyachok2005@gmail.com>
2025-12-17 15:08:29 +0200

    Add opening files w/ krita

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  "sindrets/diffview.nvim",
+  opts = {},
+
+  keys = {
+    { "<leader>dv", "<cmd>DiffviewOpen<cr>", desc = "DiffView: open" },
+    { "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "DiffView: close" },
+  },
+}
